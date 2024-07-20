1d6d7c1c76f0019458a7405b5a065909281f8165
Your Name <fikirwalle.tech@gmail.com>
2024-07-20 11:49:02 -0400

    weekend_practice_1

--- arrayBasic.py
+++ arrayBasic.py
@@ -1,4 +1,10 @@
 
-print("Hello")
 
-print("git push 1 from mac")
+def distnictArray(arrays:list[int]):
+    uniqueValues={ abs(val)for val in arrays}
+
+    if len(uniqueValues)<len(arrays):
+        return False
+    return True
+
+print(distnictArray([1,2,3,4]))
